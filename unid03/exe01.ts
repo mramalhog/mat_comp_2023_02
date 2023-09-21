55b517a9153bde8b1b4cfe5869b4197fd6a9acf0
mramalhog <marirg28@gmail.com>
2023-09-20 21:36:05 -0300

    unid03

---
+++ unid03/exe01.ts
@@ -0,0 +1,18 @@
+/*3.1. Nos argumentos seguintes, identifique as premissas e a conclusão. Reescreva os
+argumentos de modo a deixar o processo de inferência claro, fluindo das premissas para a
+conclusão.
+a) Alguns usuários acessam o sistema após as 23h. Todos os acessos após as 23h tem baixo tempo de espera. Dessa forma alguns usuários tem baixo tempo de espera.
+P1 = Alguns Usuários acessam o sistema após as 23h.
+P2 = Todos os acessos após as 23h tem baixo tempo de espera.
+Conclusão = Alguns Usuários tem baixo tempo de espera.
+
+b) Percebe-se que todos os objetos que estão marcados com as etiquetas (TxyE) no estoque foram entregues pela transportadora Txy.
+P1 = A transportadora Txy etiqueta as entregas.
+P2 = Existem objetos com etiquetas Txye.
+Conclusão = Os objetos entregues pela transportadora txy.
+
+c) O fato do log do sistema apresentar um grande volume de dados registrados significa que houve um grande número de tentativas de violação de segurança.
+P1 = Uma tentativa de violação é registrada no log.
+P2 = O log do sistema apresenta um grande volume de dados registrados.
+Conclusão = Houve um grande número de tentativas de violação de segurança.*/
+//Quando há palavras como significa, quer dizer, é conclusão.
---
+++ unid03/exe03.ts
@@ -0,0 +1,9 @@
+/*3.3. Suponha que você foi contratado para efetuar uma análise de dados para ajudar a entender o perfil dos consumidores de um mercado varejista. Explique como você faria, em
+termos de consulta de dados em uma base de dados, para dar suporte ao seguinte argumento que subsidia uma estratégia para impulsionar as vendas: Todos os consumidores que adquirem café, também adquirem leite. Todos os consumidores
+que compram adoçante, também adquirem café. Logo, todos os consumidores que adquirem adoçante, também adquirem leite. Logo, uma boa estratégia para promover a compra dos
+adoçantes é fornecer desconto para o leite e o café sempre que houver promoções de adoçantes.
+Na sua resposta, explique:
+a) A lógica da conclusão do argumento utilizando diagramas de Venn.
+b) Como a conclusão referente à validade ou não do argumento pode ser comprovada a partir dos dados presentes em um banco de dados de transações.
+c) Como isso é feito considerando a base de dados fornecida pelo professor, que apresenta dados de transações estruturados da seguinte forma:*/
+P1 = 
